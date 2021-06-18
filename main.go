80e0684f3800626d9ef2bf555cd22fee548cc1b5
Alesson Viana <alesson.viana@gmail.com>
2021-06-18 21:17:07 +0000

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+)
 
 func main() {
-  fmt.Println("Hello, World!")
+    http.HandleFunc("/", HelloServer)
+    http.ListenAndServe(":8080", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
