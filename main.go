60c20085e9f4ad21aa047db81552033d148b5a4b
Alesson Viana <alesson.viana@gmail.com>
2021-06-18 21:23:31 +0000

    Update main.go

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
     http.HandleFunc("/", HelloServer)
-    http.ListenAndServe(":8080", nil)
+    http.ListenAndServe(":80", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+    fmt.Fprintf(w, "Hello Word, %s!", r.URL.Path[1:])
 }
