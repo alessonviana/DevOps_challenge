17dc2b2929b0ef58e1aabdc3b3d2edb50df18dc9
Alesson Viana <alesson.viana@gmail.com>
2021-06-18 21:15:09 +0000

    Update main.go

--- main.go
+++ main.go
@@ -1,27 +1,7 @@
 package main
 
-import (
-    "fmt"
-    "math/rand"
-    "net/http"
-    "strconv"
-)
+import "fmt"
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        points := rand.Intn(40)
-        var m string
-        if points > 1 {
-            m = "points"
-        } else {
-            m = "point"
-
-        }
-        fmt.Fprint(w, "Hey! You win " + strconv.Itoa(points)+" "+m)
-
-    })
-
-    http.ListenAndServe("0.0.0.0:8080", nil)
-
-
+  fmt.Println("Hello, World!")
 }
