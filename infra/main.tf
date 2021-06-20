7dedaa2a0ecf34dc4d7b8a2f44036f028c4be2c4
alesson.v <alesson.viana@gmail.com>
2021-06-20 10:32:26 -0300

    fix main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -3,8 +3,8 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_key_pair" "key_name" {
-  key_name = "challenge_key"
+resource "aws_key_pair" "challenge_key" {
+ 
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAnj7eLHvt6J+N2XlwpmeMqw+k/9olK9rYNPF9FFE5LbyxoxgJCsjgYxzx174rkFrQUDi/XIvp0CC82UHtjG7a9WztLqgweFl+LC6pYG4Fi8FxacqB8eFss5fzZOdZCUMEUHg7MSzJAi5ILYM6i55kgOkv+1alvPdlkmcEs4aRzp6BfruDdxGoaYKf5ebhBNcT30hdO4rkSLBsGCFKwO4M5CzB3d0MsnC+mWT6eIX+VLSXbpxD27k8ZLGeZyx7/mNd9M6JG2SAHg1x9ULL6wQ+W1nz28BGqrg842SxoGixwNdR7aoGlfHUxclmtxi3QMuK2u/+LKAZndcU2ZvmMAJUjw== rsa-key-20201202"  
 }
 
