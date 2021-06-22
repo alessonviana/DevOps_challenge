b85bf462d9b9d371a06d0d10409459aa65db40ad
alesson.v <alesson.viana@gmail.com>
2021-06-22 16:14:49 -0300

    change drone.yml

--- infra/main.tf
+++ infra/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 resource "aws_key_pair" "challenge_key" {
  
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCkT8O3l2hC3ibozCpEeZmBOzZmZyxTBWUfSSmDaNoKV92BirOd7TmK4jePni0UAadAlh18NYBSxQsl85vt8VMdK7z8B0xqApt5PeVTwOUiLPV5+U1ApwWbbc0KbiASqkXAyXDKWldhcZGbZWnqkFwR9NOP7+Nf+OwuuxNTQ3Vg/l/PXSXNpSUBHXes6e3XERGoT1d0fvofJajiyqiM0kEXx57V8CVTOKvXzu1/A9Ute+8uT2k/KXfP+sKj5dnyg73Itf8zxRd1zbdwk+ydKUwZQpLTRYEp4MYEZsliZgvuUhnWqL8QoHPeObXqQPcsfxWMPB/GjvzVR8a3cs6ppP3p imported-openssh-key"  
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCkT8O3l2hC3ibozCpEeZmBOzZmZyxTBWUfSSmDaNoKV92BirOd7TmK4jePni0UAadAlh18NYBSxQsl85vt8VMdK7z8B0xqApt5PeVTwOUiLPV5+U1ApwWbbc0KbiASqkXAyXDKWldhcZGbZWnqkFwR9NOP7+Nf+OwuuxNTQ3Vg/l/PXSXNpSUBHXes6e3XERGoT1d0fvofJajiyqiM0kEXx57V8CVTOKvXzu1/A9Ute+8uT2k/KXfP+sKj5dnyg73Itf8zxRd1zbdwk+ydKUwZQpLTRYEp4MYEZsliZgvuUhnWqL8QoHPeObXqQPcsfxWMPB/GjvzVR8a3cs6ppP3p"  
 }
 
 resource "aws_instance" "challenge_DevOps_Machine" {
