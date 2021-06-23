0bb316bdce14afd5f41b6cc856a6dd660caae400
alesson.v <alesson.viana@gmail.com>
2021-06-22 23:04:28 -0300

    fgsdgdsf

--- infra/security-group.tf
+++ infra/security-group.tf
@@ -3,9 +3,18 @@ resource "aws_security_group" "allow-acess" {
   description = "allow-acess"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = "${var.cdirs_acesso_remoto}"
+  }
+
+  outgress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
     cidr_blocks = "${var.cdirs_acesso_remoto}"
