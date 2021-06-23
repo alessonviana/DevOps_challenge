40862724e7263295e78403377030b91bc43c8876
alesson.v <alesson.viana@gmail.com>
2021-06-22 22:01:43 -0300

    fsdg

--- infra/main.tf
+++ infra/main.tf
@@ -3,10 +3,10 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_key_pair" "challenge_key" {
- 
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCvpJkcQjpiXpz7BQCFK64dhQp6wh7OM8wCFkQR3p4G1EaJghbtdUtfEfN1FcU/DDQihWRxzGhx8YbowG6xYXv0vb0ODD1lbKx6q5eQkTyfFGCL56cPjZpd5gCG2aEu4+57E4cQQbPEwOAqhdU/EqmJf3BUdiX/06UbGvC2jMrJUfjZUbs1mKtSqryG9n/gi/21gMg2MYGvS0uUOmiEcVMyAh+fSfqYaWCACGdiaNSeQWCn9QSwYLACO6fmBJAKDyNa3s0zMBq8mO2B8fKJvoscyTe1u/fN0ddM/jkWkqByXLhNESQ04ZNDkKkFAHOv/87khhLZ1D0lXKZWFl9B0kjV teste_infra"  
-}
+#resource "aws_key_pair" "challange_DevOps" {
+# 
+#  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCvpJkcQjpiXpz7BQCFK64dhQp6wh7OM8wCFkQR3p4G1EaJghbtdUtfEfN1FcU/DDQihWRxzGhx8YbowG6xYXv0vb0ODD1lbKx6q5eQkTyfFGCL56cPjZpd5gCG2aEu4+57E4cQQbPEwOAqhdU/EqmJf3BUdiX/06UbGvC2jMrJUfjZUbs1mKtSqryG9n/gi/21gMg2MYGvS0uUOmiEcVMyAh+fSfqYaWCACGdiaNSeQWCn9QSwYLACO6fmBJAKDyNa3s0zMBq8mO2B8fKJvoscyTe1u/fN0ddM/jkWkqByXLhNESQ04ZNDkKkFAHOv/87khhLZ1D0lXKZWFl9B0kjV teste_infra"  
+#}
 
 resource "aws_instance" "challenge_DevOps_Machine" {
   ami = "ami-042e8287309f5df03"
